344a461cb86e450ad0a413577c603b322c8be81d
Rawan M Al-Shurbaji <rawnsh2218@gmail.com>
2022-12-03 13:09:07 +0200

    second

--- main.js
+++ main.js
@@ -3,19 +3,6 @@ const router = require("./controllers");
 const sequelize = require("./database/connection");
 const csp = require('express-csp-header');
 
-// app.use(router , csp({
-//     policies: {
-//         'default-src': [csp.NONE],
-//         'img-src': [csp.SELF],
-//     }
-// }));
-
-// app.use(function(req, res, next) {
-//     res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com");
-//     return next();
-// });
-
-
 const app = express();
 
 // app.use(router); 
